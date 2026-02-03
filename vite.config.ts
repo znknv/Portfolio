234a4da43ffdc7942e5997ef6a087c3ed6f25439
znknv <bakanova.zargan2@gmail.com>
2026-02-03 15:12:07 +0100

    Deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/Portfolio/',
+  server: {
+    port: 3000,
+    host: true,
+  },
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (
+          warning.message.includes('Module level directives') ||
+          warning.message.includes('"use client"')  ||
+          warning.message.includes('"was ignored"')
+        ) {
+          return; 
+        }
+        if (warning.code === 'UNRESOLVED_IMPORT') {
+          throw new Error(`Build failed due to unresolved import:\n${warning.message}`);
+        }
+        if (warning.code === 'PLUGIN_WARNING' && /is not exported/.test(warning.message)) {
+          throw new Error(`Build failed due to missing export:\n${warning.message}`);
+        }
+        warn(warning);
+      },
+    },
+  },
+});
